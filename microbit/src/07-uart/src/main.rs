e47af2a47a73ce25b9655e659107c3202536d1b6
Jose Narvaez <goyox86@gmail.com>
2023-08-22 16:48:59 +0100

    Changes to allow rust-analyzer to pick up stuff properly. Also some work.

--- microbit/src/07-uart/src/main.rs
+++ microbit/src/07-uart/src/main.rs
@@ -1,11 +1,13 @@
 #![no_main]
 #![no_std]
 
-mod serial_setup;
+pub mod serial_setup;
+
+use core::fmt::Write;
 
 use cortex_m_rt::entry;
 use panic_rtt_target as _;
-use rtt_target::rtt_init_print;
+use rtt_target::{rprintln, rtt_init_print};
 
 use microbit::{
     hal::prelude::*,
@@ -15,8 +17,77 @@ use microbit::{
 
 use serial_setup::UartePort;
 
+// #[entry]
+// fn main() -> ! {
+//     rtt_init_print!();
+//     let board = microbit::Board::take().unwrap();
+//
+//     let mut serial = {
+//         let serial = uarte::Uarte::new(
+//             board.UARTE0,
+//             board.uart.into(),
+//             Parity::EXCLUDED,
+//             Baudrate::BAUD115200,
+//         );
+//         UartePort::new(serial)
+//     };
+//
+//     writeln!(serial, "This is a test 🤙!").unwrap();
+//     nb::block!(serial.flush()).unwrap();
+//
+//     loop {}
+// }
+
+// Receiving a byte
+// #[entry]
+// fn main() -> ! {
+//     rtt_init_print!();
+//     let board = microbit::Board::take().unwrap();
+//
+//     let mut serial = {
+//         let serial = uarte::Uarte::new(
+//             board.UARTE0,
+//             board.uart.into(),
+//             Parity::EXCLUDED,
+//             Baudrate::BAUD115200,
+//         );
+//         UartePort::new(serial)
+//     };
+//
+//     loop {
+//         let byte = nb::block!(serial.read()).unwrap();
+//         rprintln!("{}", byte as char);
+//     }
+// }
+
+// Echo server
+// #[entry]
+// fn main() -> ! {
+//     rtt_init_print!();
+//     let board = microbit::Board::take().unwrap();
+//
+//     let mut serial = {
+//         let serial = uarte::Uarte::new(
+//             board.UARTE0,
+//             board.uart.into(),
+//             Parity::EXCLUDED,
+//             Baudrate::BAUD115200,
+//         );
+//         UartePort::new(serial)
+//     };
+//
+//     loop {
+//         let byte = nb::block!(serial.read()).unwrap();
+//         write!(serial, "Client sent: {}", byte).unwrap();
+//         nb::block!(serial.flush()).unwrap();
+//     }
+// }
+
+// Alright, next let's make the server more interesting by having it respond to the client with the reverse of the text that they sent.
+// The server will respond to the client every time they press the ENTER key. Each server response will be in a new line.
 #[entry]
 fn main() -> ! {
+    const ENTER_KEY: u8 = 13;
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
@@ -30,8 +101,18 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
-    nb::block!(serial.write(b'X')).unwrap();
-    nb::block!(serial.flush()).unwrap();
-
-    loop {}
+    let mut buffer: heapless::Vec<u8, 1024> = heapless::Vec::new();
+    loop {
+        let byte = nb::block!(serial.read()).unwrap();
+        if byte != ENTER_KEY {
+            buffer.push(byte).unwrap();
+        } else {
+            write!(serial, "Reversed client message: ").unwrap();
+            for byte in buffer.iter().rev().chain(&[b'\n', b'\r']) {
+                nb::block!(serial.write(*byte)).unwrap();
+            }
+            nb::block!(serial.flush()).unwrap();
+            buffer.clear();
+        }
+    }
 }
